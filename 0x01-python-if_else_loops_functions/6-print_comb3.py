555f0a0fa6dac461d2a1b03b7f4abd18a82fc303
Bi-oluwatife <osunlajaboluwatife@gmail.com>
2022-12-02 14:31:07 +0100

    6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for n in range(0, 89):
-    if (n / 10 < n % 10):
-        print("{:02d}" .format(n), end=', ')
+for n in range(0, 8):
+    for k in range(0, 10):
+        if n < k:
+            print("{}{}" .format(n, k), end=', ')
 print(89)
