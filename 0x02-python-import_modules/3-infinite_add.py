b8a2c9c55dfd89c3b560874897dddb14ba76831e
Bi-oluwatife <osunlajaboluwatife@gmail.com>
2022-12-12 05:06:54 +0100

    afresh

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    num_args = len(argv)
-    total = 0
-    for n in range(1, num_args):
-        total += int(argv[n])
-    print(f"{total:d}")
